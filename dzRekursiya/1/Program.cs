1667d1389368f11150231eb32291301abab5e3d8
ElenaK313 <152060542+ElenaK313@users.noreply.github.com>
2023-12-25 17:19:09 +0300

    ReKursiya

---
+++ dzRekursiya/1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая 
+// выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+System.Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+void PrintNumbers(int firstNum, int secondNum) 
+{
+    System.Console.Write(firstNum + ", ");
+    if(firstNum == secondNum)
+    {
+        return;
+    }
+    PrintNumbers(firstNum+1, secondNum);  
+}
+
+PrintNumbers(M,N);
---
+++ dzRekursiya/3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+int[] array = new int[]{1,2,5,0,10,34};
+int num = array.Length - 1;
+
+void ReverseArray(int[] array2, int index) 
+{
+    if(index < 0)
+    {
+        return;
+    }
+    Console.Write(array2[index]+ " ");
+    ReverseArray(array2, index-1);  
+}
+ReverseArray(array, num);
+
+
